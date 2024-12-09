8fffb462aec01e85076bb3fe85914f4040ff3573
facuso162 <facu.sosa.gabriel@gmail.com>
2024-12-09 14:24:58 -0300

    change: make the components compatible with fetched startups

--- src/app/components/StartupCard.tsx
+++ src/app/components/StartupCard.tsx
@@ -1,53 +1,58 @@
 import Image from 'next/image'
 import { Eye } from 'lucide-react'
-import { Post } from '../types/post'
 import Link from 'next/link'
+import { STARTUPS_QUERYResult } from '@/src/sanity/types'
 
 type StartupCardProps = {
-  post: Post
+  startup: STARTUPS_QUERYResult[number]
 }
 
-function StartupCard({ post }: StartupCardProps) {
-  //todo: se puede hacer destructuring de las props del post
+function StartupCard({ startup }: StartupCardProps) {
+  const { _createdAt, author, category, description, image, slug, title, views } = startup
 
   return (
     <article className='bg-white w-full border-4 border-black rounded-3xl text-black py-6 px-5 shadow-200 hover:border-primary transition-all duration-500 hover:shadow-300 hover:bg-primary-100 max-w-72 flex flex-col gap-2'>
       <header className='flex justify-between'>
         <div className='bg-primary-100 px-4 py-2 rounded-full'>
-          <span className='font-medium'>{post._createdAt}</span>
+          <span className='font-medium'>{_createdAt}</span>
         </div>
         <div className='flex items-center gap-1'>
           <Eye size={20} className='text-primary' />
-          <span className='font-medium'>{post.views}</span>
+          <span className='font-medium'>{views}</span>
         </div>
       </header>
       <div className='flex justify-between items-center'>
         <div>
-          <Link href={`/user/${post.author._id}`}>
-            <h4 className='text-sm font-medium line-clamp-1'>{`${post.author.firstName} ${post.author.lastName}`}</h4>
+          <Link href={`/user/${author?.id}`}>
+            <h4 className='text-sm font-medium line-clamp-1'>{`${author?.name}`}</h4>
           </Link>
-          <Link href={`/startup/${post._id}`}>
-            <h3 className='text-xl font-semibold line-clamp-1'>{post.title}</h3>
+          <Link href={`/startup/${slug}`}>
+            <h3 className='text-xl font-semibold line-clamp-1'>{title}</h3>
           </Link>
         </div>
         <div className='w-10 h-10 relative'>
-          <Link href={`/user/${post.author._id}`}>
-            <Image src={post.author.profileImage} fill alt='profile pic' />
+          <Link href={`/user/${author?.id}`}>
+            <Image src={author?.image || './default-profile-photo.png'} fill alt='profile pic' />
           </Link>
         </div>
       </div>
-      <p className='line-clamp-2 text-black-100 font-normal'>{post.description}</p>
+      <p className='line-clamp-2 text-black-100 font-normal'>{description}</p>
       <div className='w-full h-40 relative'>
-        <Link href={`/startup/${post._id}`}>
-          <Image src={post.image} alt='startup image' fill className='rounded-xl' />
+        <Link href={`/startup/${slug}`}>
+          <Image
+            src={image || './default-image.png'}
+            alt='startup image'
+            fill
+            className='rounded-xl'
+          />
         </Link>
       </div>
       <footer className='flex justify-between items-center'>
-        <Link href={`/?query=${post.category.toLocaleLowerCase()}`}>
-          <span className='font-medium text-base'>{post.category}</span>
+        <Link href={`/?query=${category?.toLocaleLowerCase()}`}>
+          <span className='font-medium text-base'>{category}</span>
         </Link>
         <Link
-          href={`/?query=${post.category.toLocaleLowerCase()}`}
+          href={`/?query=${category?.toLocaleLowerCase()}`}
           className='bg-black-200 rounded-full px-4 py-2 text-base font-medium text-white'>
           Details
         </Link>
--- src/app/components/Startups.tsx
+++ src/app/components/Startups.tsx
@@ -1,30 +1,30 @@
+import { STARTUPS_QUERYResult } from '@/src/sanity/types'
 import StartupCard from './StartupCard'
-import { Post } from '../types/post'
 import { Frown } from 'lucide-react'
 
 type StartupsProps = {
   query: string
-  posts: Post[]
+  startups: STARTUPS_QUERYResult
 }
 
-function Startups({ query, posts }: StartupsProps) {
+function Startups({ query, startups }: StartupsProps) {
   return (
     <section className='py-6'>
       <h2 className='text-black font-semibold text-lg px-4 md:px-16'>
         {query !== '' ? `Search results for "${query}"` : `All Startups`}
       </h2>
 
-      {posts.length > 0 ? (
+      {startups.length > 0 ? (
         <ul className='card-grid justify-center py-6 px-4'>
-          {posts.map(post => (
-            <li key={post._id} className='flex justify-center w-fit'>
-              <StartupCard post={post} />
+          {startups.map(startup => (
+            <li key={startup.slug} className='flex justify-center w-fit'>
+              <StartupCard startup={startup} />
             </li>
           ))}
         </ul>
       ) : (
         <div className='flex flex-col gap-4 justify-center items-center py-10'>
-          <Frown className='text-black-100' size={40} strokeWidth={1}/>
+          <Frown className='text-black-100' size={40} strokeWidth={1} />
           <p className='text-black-100 font-normal text-lg'>No startups found</p>
         </div>
       )}
