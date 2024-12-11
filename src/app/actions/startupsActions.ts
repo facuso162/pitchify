b0b13d8ed2dd9b0dfe2f988200a96caa42ec03f7
facuso162 <facu.sosa.gabriel@gmail.com>
2024-12-11 17:02:24 -0300

    add: create action to fetch startup details

--- src/app/actions/startupsActions.ts
+++ src/app/actions/startupsActions.ts
@@ -1,9 +1,14 @@
 'use server'
 
-import { STARTUPS_QUERY } from '@/src/sanity/lib/queries'
+import { STARTUPS_QUERY, STARTUP_DETAILS_QUERY } from '@/src/sanity/lib/queries'
 import { client } from '@/src/sanity/lib/client'
 
 export const getStartupsAction = async (startupsParams: { q: string | null }) => {
   const startups = await client.fetch(STARTUPS_QUERY, startupsParams)
   return startups
+}
+
+export const getStartupDetailsAction = async (slug: string) => {
+  const startup = await client.fetch(STARTUP_DETAILS_QUERY, { slug })
+  return startup
 }
