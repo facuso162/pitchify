56d640e80bc75d15b293a191f8dc650795726e97
facuso162 <facu.sosa.gabriel@gmail.com>
2024-12-24 12:36:43 -0300

    add: implement sanity write client

---
+++ src/sanity/lib/write-client.ts
@@ -0,0 +1,13 @@
+import 'server-only'
+
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId, writeToken } from '../env'
+
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token: writeToken,
+})
