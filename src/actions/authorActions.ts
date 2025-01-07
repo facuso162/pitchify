8a4c6d2c5ba62a7e9efe16de22050dc7b860c19c
facuso162 <facu.sosa.gabriel@gmail.com>
2025-01-07 18:39:57 -0300

    change: encapsulate author mutations in server actions

--- src/actions/authorActions.ts
+++ src/actions/authorActions.ts
@@ -1,9 +1,74 @@
 'use server'
 
 import { client } from '@/src/sanity/lib/client'
-import { AUTHOR_BY_ID_QUERY } from '@/src/sanity/lib/queries'
+import { writeClient } from '@/src/sanity/lib/write-client'
+import { AUTHOR_BY_ID_QUERY, AUTHOR_BY_AUTHPROVIDER_ID_QUERY } from '@/src/sanity/lib/queries'
+import { uploadImageAction } from './imageActions'
+import { SanityAssetDocument } from 'next-sanity'
 
 export const getAuthorAction = async (authorID: string) => {
   const author = await client.fetch(AUTHOR_BY_ID_QUERY, { authorID })
   return author
+}
+
+export const getAuthorByAuthProviderIDAction = async (
+  authProviderID: string | null,
+  useCdn: boolean = false
+) => {
+  const author = await client.withConfig({ useCdn }).fetch(AUTHOR_BY_AUTHPROVIDER_ID_QUERY, {
+    authProviderID,
+  })
+
+  return author
+}
+
+type AuthorCreationData = {
+  authProviderID?: string
+  name: string
+  email: string
+  image: File | null
+}
+
+export const createAuthorAction = async ({
+  authProviderID,
+  name,
+  email,
+  image,
+}: AuthorCreationData) => {
+  let asset: SanityAssetDocument | null = null
+  if (image !== null) {
+    asset = await uploadImageAction(image, name)
+  }
+
+  const newAuthor = {
+    _type: 'author',
+    authProviderID,
+    name,
+    username: email.split('@')[0], // TODO - Validar que el username no haya sido usado
+    email,
+    bio: '',
+    image: asset
+      ? {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: asset._id,
+          },
+        }
+      : null,
+  }
+
+  await writeClient.create(newAuthor)
+}
+
+type AuthorUpdates = {
+  name?: string
+  username?: string
+  email?: string
+  bio?: string
+}
+
+export const updateAuthorAction = async (authorID: string, authorUpdates: AuthorUpdates) => {
+  const updatedAuthor = await writeClient.patch(authorID).set(authorUpdates).commit()
+  return updatedAuthor
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,7 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
-import { client } from '@/src/sanity/lib/client'
-import { writeClient } from './sanity/lib/write-client'
-import { AUTHOR_BY_AUTHPROVIDER_ID_QUERY } from './sanity/lib/queries'
-import { SanityAssetDocument } from 'next-sanity'
 import type { Provider } from 'next-auth/providers'
+import { getAuthorByAuthProviderIDAction, createAuthorAction } from './actions/authorActions'
 
 const providers: Provider[] = [Google]
 
@@ -25,9 +22,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       if (!profile.name || !profile.sub || !profile.email) throw new Error('Invalid profile data')
 
-      const author = await client.fetch(AUTHOR_BY_AUTHPROVIDER_ID_QUERY, {
-        authProviderID: profile.sub,
-      })
+      const author = await getAuthorByAuthProviderIDAction(profile.sub)
 
       if (author) return true
 
@@ -40,25 +35,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
       }
 
-      const newAuthor = {
-        _type: 'author',
+      await createAuthorAction({
         authProviderID: profile.sub,
         name: profile.name,
-        username: profile.email.split('@')[0],
         email: profile.email,
-        bio: '',
-        image: asset
-          ? {
-              _type: 'image',
-              asset: {
-                _type: 'reference',
-                _ref: asset._id,
-              },
-            }
-          : null,
-      }
+        image: authorImage,
+      })
 
-      await writeClient.create(newAuthor)
       return true
     },
 
@@ -70,11 +53,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // no se pasa el profile, y por lo tanto, hay que chequear que no sea
       // undefined.
       if (profile !== undefined) {
-        const author = await client
-          .withConfig({ useCdn: false })
-          .fetch(AUTHOR_BY_AUTHPROVIDER_ID_QUERY, {
-            authProviderID: profile.sub || null,
-          })
+        const author = await getAuthorByAuthProviderIDAction(profile.sub || null, true)
 
         token.authorID = author ? author.authorID : null
       }
